ed546065d30af1e80602e7c1a8d723921cc8eee6
Dimitri Rusin <dimitri@habimm.com>
2024-03-09 15:37:38 +0100

    Battery Zeug.

--- battery_monitor.sh
+++ battery_monitor.sh
@@ -3,4 +3,4 @@
 # Get the current battery percentage
 battery_level=`cat /sys/class/power_supply/CMB0/capacity`
 
-case $battery_level in 11|10) pm-hibernate;; esac
+case $battery_level in 11|10|9|8|7|6|5|4|3|2|1) pm-hibernate;; esac
